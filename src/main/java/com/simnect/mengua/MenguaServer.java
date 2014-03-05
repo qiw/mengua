6af76c3d7d218610a71c50fe786b883a132b8522
James Huang <ctrlhxj@gmail.com>
2014-03-04 22:24:42 -0800

    init push

---
+++ src/main/java/com/simnect/mengua/MenguaServer.java
@@ -0,0 +1,32 @@
+package com.simnect.mengua;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import java.io.IOException;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+ 
+public class MenguaServer extends AbstractHandler
+{
+    public void handle(String target,
+                       Request baseRequest,
+                       HttpServletRequest request,
+                       HttpServletResponse response) 
+        throws IOException, ServletException
+    {
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+        baseRequest.setHandled(true);
+        response.getWriter().println("<h1>Hello World</h1>");
+    }
+ 
+    public static void main(String[] args) throws Exception
+    {
+        Server server = new Server(8081);
+        server.setHandler(new MenguaServer());
+  
+        server.start();
+        server.join();
+    }
+}
