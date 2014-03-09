564a1b420bbbb7ffe52e66d2ddd33ea383018305
qiw <wangqibeyond0226@hotmail.com>
2014-03-09 13:45:16 -0700

    Game entity

---
+++ src/main/java/com/simnect/mengua/entities/Game.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package com.simnect.mengua.entities;
+
+/**
+ * @author kwang
+ *
+ */
+public class Game {
+
+	public Game(long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	private long id;
+	private String name;
+	private String desc;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDesc() {
+		return desc;
+	}
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	
+	@Override
+	public String toString() {
+		return "Game [id=" + id + ", name=" + name + ", desc=" + desc + "]";
+	}
+	
+}
